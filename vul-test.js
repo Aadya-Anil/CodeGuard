4b7bb4338404259bada7aff7fed3f3c5d73ef8e3
Anil Kumar <aadya.anil.kumar@accenture.com>
2025-04-18 12:21:10 +0530

    Test: added vulnerable pattern to vul-test.js

--- vul-test.js
+++ vul-test.js
@@ -1,3 +1,17 @@
-const input = "' OR 1=1 --"; 
-const query = `SELECT * FROM users WHERE username = '${input}'`;
-console.log(query);
+// Simulating vulnerable code with SQL Injection
+const userInput = "' OR 1=1 --"; // Malicious input that will bypass authentication
+
+// Unsafe SQL query construction
+const query = `SELECT * FROM users WHERE username = '${userInput}' AND password = 'password'`;
+
+console.log("Generated Query: ", query);
+
+// Assume query is executed in a real database (this is just for simulation)
+const database = [
+  { username: 'admin', password: 'password' },
+  { username: 'user', password: 'userpassword' },
+];
+
+// Simulating the query execution
+const result = database.filter(user => user.username === userInput.split(' ')[0]);
+console.log("Query Result: ", result);
